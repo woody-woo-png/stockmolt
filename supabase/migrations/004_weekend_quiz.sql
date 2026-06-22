43958247fd068699b08183950e3600943308dffb
woody-woo-png <woody-woo-png@users.noreply.github.com>
2026-06-22 13:37:06 +0900

    chore(db): add weekend_coins + last_weekend_quiz_date to game_players

---
+++ supabase/migrations/004_weekend_quiz.sql
@@ -0,0 +1,7 @@
+-- supabase/migrations/004_weekend_quiz.sql
+-- weekend_coins: 주말 퀴즈 플레이로 획득한 코인 누적값
+-- last_weekend_quiz_date: 같은 날 중복 제출 방지용 날짜
+
+ALTER TABLE game_players
+  ADD COLUMN IF NOT EXISTS weekend_coins          INT  NOT NULL DEFAULT 0,
+  ADD COLUMN IF NOT EXISTS last_weekend_quiz_date DATE;
